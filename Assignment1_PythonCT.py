1c32cda04f2c1a8585e8d248daba643355f49d41
Martin Perez <116379300+mperez569@users.noreply.github.com>
2024-07-22 14:29:56 -0500

    Update Assignment1_PythonCT.py

--- Assignment1_PythonCT.py
+++ Assignment1_PythonCT.py
@@ -14,3 +14,26 @@ if emotion == "happy":
   print("That's great to hear!")
 elif emotion == "sad":
   print("I hope your day gets better!")
+
+#2. Python Naming Convention Practice
+#Task 1: Code Correction
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+#3. Arithmetic Operations in Daily Life
+#Task 1: Grocery Store Math
+bread, eggs, milk = 4, 6, 3
+tax = 1.1025
+
+result = (bread + eggs + milk) * tax
+print(result)
+
+#Task 2: Bank Interest
+init_savings = 5000
+int_rate = 0.04
+
+#amount after a year and a 7% interest rate
+f_savings = init_savings + (init_savings * int_rate)
+print(f_savings)
