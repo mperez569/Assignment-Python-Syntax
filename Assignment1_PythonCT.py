03fdc5426d194d101a5fac6d3511886c99b7f687
Martin Perez <116379300+mperez569@users.noreply.github.com>
2024-07-22 14:20:27 -0500

    Update Assignment1_PythonCT.py

--- Assignment1_PythonCT.py
+++ Assignment1_PythonCT.py
@@ -1,1 +1,16 @@
 #1. Showcase Your Dance Moves!
+#Task 1: Code Correction 
+weather = "sunny"
+
+if weather == "sunny":
+  print("Wear sunglasses!")
+else:
+  print("Take an umbrella!")
+
+#Task 2: Your Mood Today 
+emotion = input("How are you feeling today?: ")
+
+if emotion == "happy":
+  print("That's great to hear!")
+elif emotion == "sad":
+  print("I hope your day gets better!")
